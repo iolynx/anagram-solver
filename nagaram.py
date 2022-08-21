8523294cd5ab5706e077520903a8ef1a8f8c57d8
violynks <66084094+violynks@users.noreply.github.com>
2022-08-21 23:39:30 +0530

    Update nagaram.py

--- nagaram.py
+++ nagaram.py
@@ -20,14 +20,11 @@ p = []
 for i in range(3, 8):
     p.append(itertools.permutations(letters, i))
     
-print("reached")
 
 for k in range(len(p)):
     for i in list(p[k]):
         if d.check(''.join(i)) and ''.join(i) not in final_list:
             final_list.append(''.join(i))
-print(final_list)
-print("reached final")
 '''
 for i in reversed(final_list):
     pg.write((i), interval = 0.15)
